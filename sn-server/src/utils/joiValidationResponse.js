504f1c69fb9b3dd4b14815995fdf41524ef9f46f
dario <darioemmanuelsilva@gmail.com>
2023-11-05 16:59:56 -0300

    configs

---
+++ sn-server/src/utils/joiValidationResponse.js
@@ -0,0 +1,17 @@
+function validationErrorResponse(res, errors) {
+  return res.status(400).json({
+    ok: false,
+    message: "Los datos no cumplen los requisitos",
+    data: {},
+    error: errors.details.map((error) => {
+      console.log(
+        "🚀 ~ file: joiValidationResponse.js:9 ~ error:errors.details.map ~ error:",
+        error
+      );
+      return { error: error.context?.label, message: error.message };
+    }),
+  });
+}
+module.exports = {
+  validationErrorResponse,
+};
