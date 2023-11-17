32dfc61d4eececc3d229207bdcf74b6f2ff131b5
dario <darioemmanuelsilva@gmail.com>
2023-11-17 18:21:55 -0300

    login & register ok

---
+++ sn-client/src/pages/LoginScreen.jsx
@@ -0,0 +1,122 @@
+import { loginAction } from "../services/actions";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../redux/userSlice";
+import { useForm } from "../hooks/useForm";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const initialForm = {
+    email: "darioemmanuelsilva@gmail.com",
+    password: "@4Password",
+  };
+  const { email, password, onInputChange } = useForm(initialForm);
+
+  const onclickLogin = async (e) => {
+    e.preventDefault();
+    const response = await loginAction({
+      email: email,
+      password: password,
+    });
+    dispatch(loginUser(response));
+    navigate("/home");
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://github.com/org-sistemas-sn/desafio-tecnico/blob/advancedPlus/assets/sn-sintesis.png?raw=true"
+          alt="Your Company"
+        ></img>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={onclickLogin}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                value={email}
+                onChange={onInputChange}
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></input>
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={onInputChange}
+                required
+                placeholder="Ejemplo: @4Password"
+                className="block w-full rounded-md border-0 py-1.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              ></input>
+            </div>
+          </div>
+
+          <div>
+            {/* {isLoading ? (
+              "loading"
+            ) : ( */}
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+            {/* )} */}
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?
+          <Link
+            to={"/register"}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
